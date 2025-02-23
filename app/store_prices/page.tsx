cefef37e6ac07c3efa03725547aa39babe4d90d0
nikola-cao <cao.nikola.a@gmail.com>
2025-02-23 04:40:56 -0500

    changes

---
+++ app/store_prices/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { ShoppingListItem } from "@/components/shopping-list-item";
+import { useSearchParams } from "next/navigation";
+
+export default function Store_prices () {
+
+	
+}
